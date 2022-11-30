68aac90d0b2bab250de2b4b60e456e164fdb7d75
junankarg <sjunankar4@gmail.com>
2022-12-01 03:06:03 +0530

    prime and palindrom file

---
+++ prime.js
@@ -0,0 +1,11 @@
+let fact=0;
+for(let i=0; i<Number; i++){
+    if(Number%i==1){
+        fact++;
+    }
+    if(fact==2){
+        console.log("Yes");
+    }else{
+        console.log("No");
+    }
+}
